76291b4e0744d21c94c74a3f94d6c840f28bd8bf
quackness <dubajkaro@gmail.com>
2020-12-07 01:32:30 +0000

    end of week 4

--- app/actions.rb
+++ app/actions.rb
@@ -6,3 +6,31 @@ get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
 end
+
+get '/signup' do # if user navigates to the path /signup
+    @user = User.new # setup empty @user object
+    erb(:signup)  # render from "app/views/signup.erb"
+end
+
+post '/signup' do
+    # grab user input values from params 
+    email = params[:email]
+    avatar_url = params[:avatar_url]
+    username = params[:username]
+    password = params[:password]
+
+    # if all user params are present
+    # deactivated if email.present? && avatar_url.present? && username.present? && password.present?
+
+    #instantite and save a user
+    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+    
+    if @user.save
+        "User #{username} saved!"
+
+    # return readable representation of User object
+    # deactivated escape_html user.inspect
+    else
+        erb(:signup)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
     has_many :comments
     has_many :likes
 
+    # code added Dec 2nd 
+    validates :email, :username, uniqueness: true
+    validates :email, :avatar_url, :username, :password, presence: true
+
 end
