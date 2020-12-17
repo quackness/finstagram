b74e81ce6898f19c8a31ccefaef4b47206688df3
quackness <dubajkaro@gmail.com>
2020-12-17 01:08:15 +0000

    changes for deployment

--- app/actions.rb
+++ app/actions.rb
@@ -64,3 +64,54 @@ get '/logout' do
     session[:user_id] = nil
     redirect to('/')
 end
+
+get '/finstagram_posts/new' do
+    @finstagram_post = FinstagramPost.new
+    erb(:"finstagram_posts/new")
+end
+
+post '/finstagram_posts' do
+    photo_url = params[:photo_url]
+    # instantiate new finstagram post
+    @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
+    # if @post validates, save
+    if @finstagram_post.save
+        redirect(to('/'))
+    else
+        erb(:"finstagram_posts/new")
+    end
+end
+
+get '/finstagram_posts/:id' do
+    @finstagram_post = FinstagramPost.find(params[:id]) # find the post with the id from the url
+    erb(:"finstagram_posts/show") # render app/views/finstagram_posts/show.erb
+end
+
+post '/comments' do
+    # point values from params to var.
+  text = params[:text]
+  finstagram_post_id = params[:finstagram_post_id]
+
+  #instantiate a commment with those values and assign a comment to a current user
+  comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+
+  #save the comment
+  comment.save
+  # redirect back to whererever we came from
+  redirect(back)
+end
+
+post '/likes' do
+    finstagram_post_id = params[:finstagram_post_id]
+
+    like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+    like.save
+
+    redirect(back)
+end
+
+delete '/likes/:id' do
+  like = Like.find(params[:id])
+  like.destroy
+  redirect(back)
+end
